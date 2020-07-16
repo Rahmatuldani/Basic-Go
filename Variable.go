e4c90b2967c07557f75b9fcb7401a65fddb1e535
Rahmatuldani <danirahmatul@gmail.com>
2020-07-16 13:26:43 +0700

    Tugas Variabel

---
+++ Variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var brother string = "Brother"
+	var sister string = "Sister"
+
+	fmt.Printf(":::::Output:::::\n")
+	fmt.Printf("Nama Kakak : %s\n",brother)
+	fmt.Printf("Nama Adik : %s",sister)
+}
